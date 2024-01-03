8cc9d07aee84c34700b80a971820bded08c82222
jd richards <jdrichardstech@gmail.com>
2024-01-03 16:28:08 -0500

    add server call

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  return <div>Basic Pursuit React Starter</div>;
+  const [fullCohort, setFullCohort] = useState([])
+
+
+  useEffect(() => {
+    (async function () {
+      const res = await fetch('http://localhost:5001/api/students')
+      const data = await res.json()
+      setFullCohort(data)
+    })()
+  }, [])
+
+  return <div>{console.log(fullCohort)}
+    <h1>Hello</h1>
+  </div>
 };
 
 export default App;
