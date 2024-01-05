130222d49a144acae7db3ab526944db4554c3d3a
jd richards <jdrichardstech@gmail.com>
2024-01-05 09:05:44 -0500

    fix url

--- src/Details.jsx
+++ src/Details.jsx
@@ -4,15 +4,13 @@ import { useEffect, useState } from 'react'
 export default function Details({ fullCohort }) {
     const [student, setStudent] = useState({})
     const { id } = useParams()
+    const URL = import.meta.env.VITE_BASE_API_URL
 
-
-    // const { username } = foundStudent
-    // console.log(foundStudent)
 
     useEffect(() => {
-        fetch(`http://localhost:5001/api/students/${id}`).then(res => res.json()).then(data => setStudent(data))
+        fetch(`${URL}/api/students/${id}`).then(res => res.json()).then(data => setStudent(data))
     }, [id])
 
-    // return null
+
     return <div><h1>{student.username}</h1><Link to={`/students/${id}/edit`}>Edit</Link></div>
 }
